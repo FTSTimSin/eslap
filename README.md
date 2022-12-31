a9ece6146391cadf8559ad89741e7ba097529c75
FTSTimSin <timsin999@gmail.com>
2022-12-31 13:46:15 -0800

    Alpha 3: Added Curves

--- README.md
+++ README.md
@@ -3,6 +3,11 @@ Makes random lines and shapes
 
 ## Changelog
 ---
+### Alpha 3: The Sexy Update (12/31/2022)
+- Added curve slider that controls the frequency of curved lines.
+- Changed most frequency sliders to be probability based.
+- Changed default/max values for sliders.
+
 ### Alpha 2: The Diagonal Update (12/31/2022)
 - Added diagonal slider that controls the frequency of pure horizontal/vertical lines.
 - Improved UI.
--- index.js
+++ index.js
@@ -6,36 +6,45 @@ $(() => {
   const generateBtn = $('#generate');
   const frequencySlider = $('#frequency-slider');
   const diagonalSlider = $('#diagonal-slider');
+  const curveSlider = $('#curve-slider')
 
   generateBtn.on('click', () => {
     const freq = frequencySlider.val();
-    const diagonalPercent = diagonalSlider.val();
-
-    const orthogonalFreq = freq * (diagonalPercent / 100);
-    const diagonalFreq = freq - orthogonalFreq;
-
-    console.warn(orthogonalFreq, diagonalFreq);
+    const diagonalPercent = diagonalSlider.val() / 100;
+    const curvePercent = curveSlider.val() / 100;
 
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (let i = 0; i < diagonalFreq; i++) {
-      const isVertical = Math.random() > 0.5;
-      context.beginPath();
+    for (let i = 0; i < freq; i++) {
+      const isDiagonal = Math.random() < diagonalPercent;
+      const isCurve = Math.random() < curvePercent;
       const startingPoint = [Math.random() * canvas.width, Math.random() * canvas.height]
+      let endingPoint;
+
+      context.beginPath();
       context.moveTo(...startingPoint);
-      if (isVertical) {
-        context.lineTo(Math.random() * canvas.width, startingPoint[1]);
+      if (isDiagonal) {
+        endingPoint = [Math.random() * canvas.width, Math.random() * canvas.height]
       } else {
-        context.lineTo(startingPoint[0], Math.random() * canvas.height);
+        if (Math.random() > 0.5) {
+          // Vertical Line
+          endingPoint = [Math.random() * canvas.width, startingPoint[1]];
+        } else {
+          // Horizontal Line
+          endingPoint = [startingPoint[0], Math.random() * canvas.height]
+        }
+      }
+
+      if (isCurve) {
+        context.quadraticCurveTo(
+          Math.random() * canvas.width, 
+          Math.random() * canvas.height,
+          ...endingPoint
+        )
+      } else {
+        context.lineTo(...endingPoint);
       }
       context.stroke();
     }
-
-    for (let i = 0; i < orthogonalFreq; i++) {
-      context.beginPath();
-      context.moveTo(Math.random() * canvas.width, Math.random() * canvas.height);
-      context.lineTo(Math.random() * canvas.width, Math.random() * canvas.height);
-      context.stroke();
-    }
-  })
+  });
 });
